b9dadb49226ec30503cf81242a99ef0a2d5ce8f3
최도솔 <dosa1572@gmail.com>
2015-01-13 14:41:30 +0900

    bsd적용 내용 수정

--- backends/generic-php.be/master-files/lsuc-master-files/bsd/bsd-c6-inc-master.php
+++ backends/generic-php.be/master-files/lsuc-master-files/bsd/bsd-c6-inc-master.php
@@ -32,35 +32,24 @@ $gOsLicTlExplanationChapter="6.4.10";
 $gFbLiSpUcName="BSD-C6";
 
 $gFbLiSpUcDesc=
-"that you received a BSD licensed code snippet, module, library,
-  or plugin (snimoli), that you modified it, and that you are now going to
-  distribute this modified version to third parties in the form of source code
-  files or as a source code package, but without embedding it into another
-  larger software unit.";
+"BSD로 라이선스 된 받은 스니펫, 모듈, 라이브러리 또는 플러그인 등 스니몰리의 수정 버전을 소스코드 파일 형태 또는 소스코드 패키지로 제3자에게 배포하려는 의도를 갖고 있다. 단, 이를 다른 더 큰 소프트웨어 유닛에 삽입하지는 않는다.";
 
 $gFbLiSpUcRequiresPrefix=
-	"the following tasks in order to fulfill the license conditions:";
+	"라이선스 조건을 충족하기 위한 작업은 다음과 같다.:";
 
 $gFbLiSpUcNoTasks="";
 
 $gFbLiSpUcRequiresManArr=array(
-		"Ensure that the licensing elements - esp. the BSD license text, the
-		specific copyright notice of the original author(s), and the BSD disclaimer
-		- are retained in your package in the form you have	received them."
+		"라이선싱하는 요소(특히, BSD 라이선스 텍스트, 원저작자의 특정 저작권 공지, BSD 권리 포기)는 받앗던 형태로 패키지에 포함한다."
 );
 
 $gFbLiSpUcRequiresVolArr=array(
-	"Let the documentation of your distribution  and/or your additional material
-	also contain the original copyright notice, the BSD conditions, and the
-	BSD disclaimer."
+	"배포 문서 및 추가자료는 저작자 특정 저작권 공지, BSD 조건, BSD 권리포기사항을 포함한다."
 );
 
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
-		"nothing explicitly if you are using the BSD 2 Clause License. But the
-		BSD 3 Clause License explicitly prohibits to use the name of the
-		licensing organization or the names of the licensing contributors to
-		promote your own work."
+		"BSD 2 Clause License를 사용하고 있다면 금지사항은 명확히 존재하지 않는다. 그러나 BSD 3 Clause License에 의해 자신의 저작물을 홍보하기 위해 라이선싱 기관명 또는 라이선싱 기여자 (licensing contributor)명 사용이 명백히 금지된다."
 		);
 
 ?>
--- backends/generic-php.be/master-files/lsuc-master-files/bsd/bsd-c7-inc-master.php
+++ backends/generic-php.be/master-files/lsuc-master-files/bsd/bsd-c7-inc-master.php
@@ -32,39 +32,23 @@ $gOsLicTlExplanationChapter="6.4.10";
 $gFbLiSpUcName="BSD-C7";
 
 $gFbLiSpUcDesc=
-"that you received a BSD licensed code snippet, module, library,
-  or plugin (snimoli), that you modified it, and that you are now going to
-  distribute this modified version to third parties in the form of binary files
-  or as a binary package but without embedding it into another larger software
-  unit.";
+"BSD로 라이선스 된 받은 스니펫, 모듈, 라이브러리 또는 플러그인 등 스니몰리의 수정 버전을 소스코드 파일 형태 또는 소스코드 패키지로 제3자에게 배포하려는 의도를 갖고 있다. 단, 이를 다른 더 큰 소프트웨어 유닛에 삽입하지는 않는다.";
 
 $gFbLiSpUcRequiresPrefix=
-	"the following tasks in order to fulfill the license conditions:";
-
+	"라이선스 조건을 충족하기 위한 작업은 다음과 같다.:";
 $gFbLiSpUcNoTasks="";
 
 $gFbLiSpUcRequiresManArr=array(
 
-	"Ensure that your distribution contains the
-  original copyright notice, the BSD license, and the BSD disclaimer in the form
-  you have received them. If you compile the binary file on the base of the
-  source code package and if this compilation does not also generate and
-  integrate the licensing files, then create the copyright notice, the BSD
-  conditions, and the BSD disclaimer according to the form of the source code
-  package and insert these files into your distribution manually.",
+	"배포물에 받았던 형태로 최초 저작권 공지, BSD 라이선스, BSD 권리포기사항을 포함한다. 소스코드 패키지에 기반하여 바이너리 파일을 컴파일하고 이 컴파일로 라이선싱 파일이 생성되거나 포함되지 않는다면, 소스코드 패키지 형태에 따라 저작권 공지, BSD 조건, BSD 권리포기사항을 작성한 다음에 이 파일들을 배포물에 수동으로 삽입한다.",
 
-  "Ensure that the documentation of your
-  distribution and/or your additional material also contain the author specific
-  copyright notice, the BSD conditions, and the BSD disclaimer."
+  "배포 문서 및 추가 자료는 저작자 특정 저작권 공지, BSD 조건, BSD 권리포가사항을 포함한다."
 
 );
 
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
-		"nothing explicitly if you are using the BSD 2 Clause License. But the
-		BSD 3 Clause License explicitly prohibits to use the name of the
-		licensing organization or the names of the licensing contributors to
-		promote your own work."
+		"BSD 2 Clause License를 사용하고 있다면 금지사항은 명확히 존재하지 않는다. 그러나 BSD 3 Clause License에 의해 자신의 저작물을 홍보하기 위해 라이선싱 기관명 또는 라이선싱 기여자 (licensing contributor)명 사용이 명백히 금지된다."
 		);
 
 ?>
