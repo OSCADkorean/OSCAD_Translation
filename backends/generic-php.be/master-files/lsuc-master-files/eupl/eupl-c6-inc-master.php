4bd6705a8df2306ccc10eb70cb1ecaf0305a011d
tnsanzz <tnsanzz@naver.com>
2015-01-13 17:28:58 +0900

    moo backends/eupl

--- backends/generic-php.be/master-files/lsuc-master-files/eupl/eupl-c6-inc-master.php
+++ backends/generic-php.be/master-files/lsuc-master-files/eupl/eupl-c6-inc-master.php
@@ -32,55 +32,42 @@ $gOsLicTlExplanationChapter="6.7.12";
 $gFbLiSpUcName="EUPL-1.1-C6";
 
 $gFbLiSpUcDesc=
-	"that you received a EUPL-1.1 licensed program, application, or
-  server (proapse), that you modified it, and that you are now going to
-  distribute this modified version to third parties in the form of source code files or as
-  a source code package.";
+	" EPL로 라이선스 된 받은 스니펫, 모듈, 라이브러리 또는 플러그인 등 스니몰리의 수정 버전을 소스코드 파일 형태 또는 소스코드 패키지로 제3자에게 배포하려는 의도를 갖고 있다. 단, 이를 다른
+더 큰 소프트웨어 유닛에 삽입하지는 않는다. ";
 
 $gFbLiSpUcRequiresPrefix=
-	"the following tasks in order to fulfill the license conditions:";
+	" 라이선스 조건을 충족하기 위한 작업은 다음과 같다.";
 
 $gFbLiSpUcNoTasks="";
 
 $gFbLiSpUcRequiresManArr=array(
 
-  "Ensure that the licensing elements
-  (particularly the copyright, patent, and trademark notices and all notices
-  that refer to the license or to the disclaimer of warranties) are retained in
-  your package in the form you have received them.",
+  "라이선싱하는 요소 특히, 저작권 공지와 법적 책임 및 보증에 대한 권리포기는 받았던
+정확한 형태로 패키지에 포함한다.",
 
-  "Give the recipient a copy of the EUPL-1.1
-  license. If it is not already part of the software package, add
-  it.",
+  "받는 사람에게 EUPL-1.1 라이선스 사본을 제공한다. 이미 구성한 바이너리 패키지의 일
+부에서 빠져있다면 이를 추가한다",
 
-  "Create a modification text file, if such a
-  notice file still does not exist. Expand the modification text
-  file by a description of your modifications.",
+  "공지 파일이 존재하지 않으면 수정 텍스트 파일을 작성한다. 보다 일반적인 수정 설명으
+로 수정 텍스트 파일을 확장한다. 이를 배포 패키지에 포함한다",
   
-  "Mark all modifications of source code of the
-  program (proapse) thoroughly within the source code and include the date of the
-  modification.",
+  "모든 소스코드의 수정사항을 프로그램(프로앱스)에 전부 표시한다. 즉 수정된 소스코드
+내에 표시한다.",
  
-  "Arrange your modifications in a way that they are
-  covered by the existing EUPL-1.1 licensing statements. If you add new source code
-  files, insert a header containing your copyright line and an EUPL-1.1 adequate
-  licensing the statement."
+  "모든 소스코드의 수정사항을 프로그램(프로앱스)에 전부 표시한다. 즉 수정된 소스코드
+내에 표시한다."
 
 );
 
 $gFbLiSpUcRequiresVolArr=array(
-  "Let the documentation of your distribution and/or
-  your additional material also reproduce the content of the existing
-  copyright notice text files, a hint to the software name, a link to its
-  homepage, and a link to the EUPL-1.1 license."
+  "배포 문서 및 추가 자료를 통해 또한 기존의 저작권 공지 텍스트 파일 내용, 소프트웨어
+명칭에 대한 힌트, 홈페이지 링크, EPL 1.0 라이선스 링크를 복제한다. "
 );
 
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
-	"to promote any of your services or products - based on the this software
-	- by trade names, trademarks, service marks, or names linked to this EUPL-1.1
-	software, except as required for reasonable and customary use in describing 
-		the origin of the software and reproducing the copyright notice."
+	"사용한 소프트웨어 파일을 공정하게 설명하고 저작권 공지를 복제하는데 필요한 경우를 제외하고
+EUPL 소프트웨어에 연결된 명칭, 상표명, 상표, 서비스 마크에 의한 서비스 또는 제품의 홍보"
 	);
 
 ?>
