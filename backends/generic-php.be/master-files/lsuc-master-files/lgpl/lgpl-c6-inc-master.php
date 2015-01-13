dd1ce1f0c956f14b32d2ec4780c5a523377c4487
junehyunBaek <100aniv62@gamil.com>
2015-01-13 16:40:48 +0900

    lgpl-c6-inc-master  - complete

--- backends/generic-php.be/master-files/lsuc-master-files/lgpl/lgpl-c6-inc-master.php
+++ backends/generic-php.be/master-files/lsuc-master-files/lgpl/lgpl-c6-inc-master.php
@@ -18,11 +18,11 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
  
-/*
+/******************
 <translation>
 date : 2015.01.13
 name : junehyunBaek 
-*/
+*******************/
 
 $gFbLicenseName="LGPLv[2.1|3.0]";
 $gFbLicenseSpecification="GNU Lesser General Public License";
