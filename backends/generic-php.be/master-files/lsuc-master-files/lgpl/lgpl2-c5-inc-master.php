d82b73049da20213bd6f32d5262d0d8febea6cc6
junehyunBaek <100aniv62@gamil.com>
2015-01-13 17:32:26 +0900

    lgpl2-c5-inc-master  -complete

--- backends/generic-php.be/master-files/lsuc-master-files/lgpl/lgpl2-c5-inc-master.php
+++ backends/generic-php.be/master-files/lsuc-master-files/lgpl/lgpl2-c5-inc-master.php
@@ -18,11 +18,11 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-/*
+/******************
 <translation>
 date : 2015.01.13
 name : junehyunBaek 
-*/
+*******************/
  
 $gFbLicenseName="LGPLv[2.1]";
 $gFbLicenseSpecification="GNU Lesser General Public License";
@@ -38,14 +38,24 @@ $gOsLicTlExplanationChapter="6.9.10";
 $gFbLiSpUcName="LGPL2-C5";
 
 $gFbLiSpUcDesc=
+
+/*
 	"that you received an LGPL-2.1 licensed program,
 application, or server (proapse), that you modified it, and that you are now
 going to distribute this modified version to third parties in the form of binary
 files or as a binary package.";
+*/
+
+"LGPL-2.1로 라이선스 된 받은 프로그램, 어플리케이션 또는 서버의 수정 버전을 (proapse) 바이너리 파일 형태 또는 바이너리 패키지로 제3자에게 배포하려는 의도를 갖고 있다.";
+
 
 $gFbLiSpUcRequiresPrefix=
+/*
 	"irrelvant because of the specific LGPL-2.1 requirement that the
   result of a modification must also be a library.";
+*/
+
+"LGPL-2.1에서는 수정된 저작물은 소프트웨어 라이브러리여아 한다 라고 명백히 요구한다.";
 
 $gFbLiSpUcNoTasks="";
 
@@ -55,8 +65,14 @@ $gFbLiSpUcRequiresVolArr=array();
 
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
+
+/*
 		"to modify the received work in a way that the resulting
 		modified work is no longer a software library but a	program"
+*/
+"결과적으로 수정된 저작물이 더 이상 소프트웨어 라이브러리(프로그램은 제외)가 아닌 방식으로 받은 저작물을 수정하는 행위이다"; 
+
+
 		);
 
 ?>
