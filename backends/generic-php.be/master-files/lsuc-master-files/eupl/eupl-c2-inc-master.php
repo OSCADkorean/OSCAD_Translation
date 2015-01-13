353be93cddcffad2a95615c7cde1a47eb1eac50e
tnsanzz <tnsanzz@naver.com>
2015-01-13 15:53:46 +0900

    moo.first misson

--- backends/generic-php.be/master-files/lsuc-master-files/eupl/eupl-c2-inc-master.php
+++ backends/generic-php.be/master-files/lsuc-master-files/eupl/eupl-c2-inc-master.php
@@ -32,43 +32,34 @@ $gOsLicTlExplanationChapter="6.7.12";
 $gFbLiSpUcName="EUPL-1.1-C2";
 
 $gFbLiSpUcDesc=
-	"that you received EUPL-1.1 licensed software which you are now going
-  to distribute to third parties as an independent unit and in the form of
-  unmodified source code files or as unmodified source code package. In this
-  case it makes no difference if you distribute a program, an application, a
-  server, a snippet, a module, a library, or a plugin as an independent or as an
-  embedded unit.";
+	" EUPL로 라이선스  받은 소프트웨어의 비수정 버전을 독립 유닛으로, 소스코드 파일 형태 또는
+소스코드 패키지로 제3자에게 배포하고자 하는 의도를 갖고 있다. 이 경우 프로그램, 어플리케이
+션, 서버, 스니펫, 모듈, 라이브러리 또는 플러그인 등 배포유형을 식별하는 것은 아니다.";
 
 $gFbLiSpUcRequiresPrefix=
-	"the following tasks in order to fulfill the license conditions:";
+	"라이선스 조건을 충족하기 위한 작업은 다음과 같다.";
 
 $gFbLiSpUcNoTasks="";
 
 $gFbLiSpUcRequiresManArr=array(
 
-  "Ensure that the licensing elements
-  (particularly the copyright, patent, and trademark notices and all notices
-  that refer to the license or to the disclaimer of warranties) are retained in
-  your package in the form you have received them.",
+  "라이선싱하는 요소, 특히 저작권, 특허 또는 상표 공지 그리고 라이선스와 보증 제한을
+뜻하는 공지 등은 받았던 형태로 패키지에 포함한다.",
 
-  "Give the recipient a copy of the EUPL-1.1  license. If it is not already
-	part of the software package, add it."
+  "받는 사람에게 EUPL-1.1 라이선스 사본을 제공한다. 이미 구성한 소프트웨어 패키지의
+일부에서 빠져있다면 이를 추가한다."
 
 );
 
 $gFbLiSpUcRequiresVolArr=array(
-  "Let the documentation of your distribution and/or
-  your additional material also reproduce the content of the existing
-  copyright notice text files, a hint to the software name, a link to its
-  homepage, and a link to the EUPL-1.1 license."
+  " 배포 문서 및 추가 자료를 통해 또한 저작권 공지 텍스트 파일 내용, 소프트웨어 명칭에
+대한 힌트, 홈페이지 링크, EUPL-1.1 라이선스 링크를 복제한다."
 );
 
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
-		"to promote any of your services or products - based on the this software
-		- by trade names, trademarks, service marks, or names linked to this EUPL-1.1
-		software, except as required for reasonable and customary use in describing 
-		the origin of the software and reproducing the copyright notice."
+		"사용한 소프트웨어 파일을 공정하게 설명하고 저작권 공지를 복제하는데 필요한 경우를 제외하고
+EUPL 소프트웨어에 연결된 명칭, 상표명, 상표, 서비스 마크에 의한 서비스 또는 제품의 홍보"
 		);
 
 ?>
