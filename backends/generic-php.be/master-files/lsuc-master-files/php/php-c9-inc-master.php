3b368e870abe2b3f42f67f53df93cf9833c3cde5
3교육실 <3@172.22.140.101>
2015-01-14 10:29:50 +0900

    buyong

---
+++ backends/generic-php.be/master-files/lsuc-master-files/php/php-c9-inc-master.php
@@ -0,0 +1,72 @@
+<?php
+
+/*  This file is part of OSCAd, the Open Source Compliance Advisor
+ *
+ *  Copyright (C) 2013 Karsten Reincke, Deutsche Telekom AG
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+$gFbLicenseName="PHP-3.0";
+$gFbLicenseSpecification="PHP-3.0 License";
+$gFbLicenseAbbr="PHP";
+$gFbLicenseRelease="3.0";
+
+$gOsLicProtectionChapter="2.13";
+$gOsLicPatentChapter="";
+$gOsLicTodoListChapter="6.14";
+$gOsLicLsucChapter="6.14.9";
+$gOsLicTlExplanationChapter="6.14.10";
+
+$gFbLiSpUcName="PHP-3.0-C9";
+
+$gFbLiSpUcDesc=
+	"PHP로 라이선스 된 받은 스니펫, 모듈, 라이브러리 또는 플러그인 등 스니몰리의 수정 버전을 바
+이너리 파일 형태 또는 바이너리 패키지로 제3자에게 내장 컴포넌트로서 스니펫, 모듈, 라이브러
+리 또는 플러그인을 포함하는 다른 더 큰 소프트웨어 유닛과 함께 배포하려는 의도를 갖고 있다.";
+
+$gFbLiSpUcRequiresPrefix=
+	"라이선스 조건을 충족하기 위한 작업은 다음과 같다.";
+
+$gFbLiSpUcNoTasks="";
+
+$gFbLiSpUcRequiresManArr=array(
+
+  "배포 문서 및 추가 자료에 “이 제품은 다음의 주소(http://www.php.net/)에서 자유로이
+이용할 수 있는 PHP를 포함한다”라는 형태로 승인 문구를 포함한다.",
+  
+  "배포 문서 및 추가 자료에 또한 초기 저작권 공지, PHP 조건 및 PHP 권리포기사항을 포
+함한다."
+);
+
+$gFbLiSpUcRequiresVolArr=array(
+ "전체 PHP 라이선스(특히, 저작권 공지, PHP 조건 및 PHP 권리포기)는 패키지로 특히, 라
+이브러리를 사용하는 자신의 지배적인 프로그램의 저작권 화면을 통해 복제한다.",
+  
+  "코드를 배포하지 않으려 해도 소스코드 안에 수정사항을 표시한다.",
+
+  "라이선싱하는 요소(특히, PHP 라이선스 텍스트, 원저작자 특정 저작권 공지 및 PHP 권리
+포기사항)가 내장 라이브러리만을 명확히 가리키고 자신의 지배적인 저작물의 라이선싱
+을 침해하지 않도록 소스코드 배포물을 구성한다. 모든 라이선싱하는 요소가 들어간 특
+정 디렉토리에 라이브러리, 모듈, 스니펫 또는 플러그인을 두는 것이 바람직하다."
+);
+
+$gFbLiSpUcForbidsPrefix="";
+$gFbLiSpUcForbidsArr=array(
+		"PHP’라는 명칭으로 단독으로 사용된 소프트웨어에 근거하여 어떠한 서비스를 보증하거나 홍
+보하는 행위"
+		);
+
+
+?>
