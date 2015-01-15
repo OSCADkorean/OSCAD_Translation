f56e20523acc2fac3629534ffe250294d6f642ea
changeun <jceun57@gmail.com>
2015-01-15 10:43:49 +0900

    jochangeun commit

--- backends/generic-php.be/snimoli/unmodified/embedded/4yourself/php-c1-inc.php
+++ backends/generic-php.be/snimoli/unmodified/embedded/4yourself/php-c1-inc.php
@@ -32,18 +32,15 @@ $gOsLicTlExplanationChapter="6.14.10";
 $gFbLiSpUcName="PHP-3.0-C1";
 
 $gFbLiSpUcDesc=
-	"that you received PHP-3.0 licensed software, that you will use it
-  only for yourself, and that you do not hand it over to any 3rd party in any
-  sense.";
+	"PHP로 라이선스 된 받은 소프트웨어를 단독으로 사용하고 제3자에게 양도하지 않는 의도를 갖고
+있다.";
 
 $gFbLiSpUcRequiresPrefix=
-	"no tasks in order to fulfill the conditions of the PHP-3.0 license
-	with respect to this use case:";
+	"다음 사용 사례에 관련하여 PHP 라이선스 조건을 충족하기 위한 작업은 없다.";
 
 $gFbLiSpUcNoTasks=
-	"You are allowed to use any kind of PHP-3.0 software in any sense and in
-    any context without any obligations as long as you do not give the software
-    to 3rd parties.";
+	"제3자에게 소프트웨어를 제공하지 않는 한 어떠한 의무가 부과되지 않는 정황에서 어떠한 유형의
+PHP 소프트웨어를 사용할 수 있다.";
 
 $gFbLiSpUcRequiresManArr=array();
 
@@ -51,8 +48,8 @@ $gFbLiSpUcRequiresVolArr=array();
 
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
-		"to endorse or promote any service you establish on the base of
-		this privately used software by the name 'PHP'."
+		"‘PHP’라는 명칭으로 단독으로 사용된 소프트웨어에 근거하여 어떠한 서비스를 보증하거나 홍
+보하는 행위"
 		);
 
 ?>
