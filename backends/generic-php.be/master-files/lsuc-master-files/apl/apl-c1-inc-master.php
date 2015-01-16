22bf971eb97c13d9bfc2de664039a4f1700de858
백준현 <100aniv62@gamil.com>
2015-01-16 13:46:23 +0900

    APL-C1: 단독으로 소프트웨어 사용  - commit

--- backends/generic-php.be/master-files/lsuc-master-files/apl/apl-c1-inc-master.php
+++ backends/generic-php.be/master-files/lsuc-master-files/apl/apl-c1-inc-master.php
@@ -17,6 +17,23 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
+ 
+/******************************************************
+<Open Source Compliance Advisor Korean Project>
+# Contributor :
+  Junehyun Baek <100aniv62@gmail.com>
+  Dosol choi    <dosa1572@gmail.com>
+  Booyong shin  <tlsqndyd@gmail.com>
+  Hoonmoo ryu   <tnsanzz@gmail.com>
+  Minhyung Cho  <designer.mhc@gmail.com>
+  Jaekyung Kim  <bonoxtwo@gmail.com>
+  Changeun Cho  <jceun57@gmail.com>
+
+# Editor : Junehyun Baek <100aniv62@gmail.com>
+# Project Period : 2015.01.12 ~ 2015.01.23
+# Edit date : 2015.01.16
+# Etc. Comment : Apache -> Apache-2.0
+*******************************************************/  
 
 $gFbLicenseName="Apache-2.0";
 $gFbLicenseSpecification="Apache License 2.0";
@@ -32,18 +49,30 @@ $gOsLicTlExplanationChapter="6.3.10";
 $gFbLiSpUcName="Apache-2.0-C1";
 
 $gFbLiSpUcDesc=
+/*
 	"that you received Apache-2.0 licensed software, that you will use it
   only for yourself and that you do not hand it over to any 3rd party in any
   sense.";
+*/
+
+	"아파치-2.0으로 라이선스 된 받은 소프트웨어를 단독으로 사용하고 제3자에게 양도하지 않으려 하는 의도를 갖고 있다.";
 
 $gFbLiSpUcRequiresPrefix=
+/*
 	"no tasks in order to fulfill the conditions
 	of the Apache 2.0 license with respect to this use case:";
+*/
+
+	"다음 사용 사례에 관련하여 아파치 2.0 라이선스 조건을 충족하기 위한 작업은 없다.";
 
 $gFbLiSpUcNoTasks=
+/*
 	"You are allowed to use any kind of Apache software in any sense
 	and in any context without being obliged to do anything as long
 	as you do not give the software to 3rd parties.";
+*/
+
+	"제3자에게 소프트웨어를 제공하지 않는 한 의무가 부과되지 않으면서 어떠한 사용 상황에서도 어떠한 유형의 아파치 소프트웨어도 사용할 수 있다."; 
 
 $gFbLiSpUcReqManArr=array();
 
@@ -51,12 +80,20 @@ $gFbLiSpUcReqVolArr=array();
 
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
+/*
 		"to promote any of your services - based on the this software - by
 		trademarks, service marks, or product names linked to the software 
 		except as required for reasonable and customary use in describing 
 		the origin of the work and reproducing the content of the NOTICE file.",
+*/
+		"사용한 소프트웨어 파일을 공정하게 설명하는데 필요한 경우를 제외하고, 활용하는 소프트웨어에 연결된 제품명, 제품 상표, 서비스 마크에 기반하여 자신의 서비스를 홍보.",
+
+/*		
 		"to institute any patent litigation against anyone alleging that the
 		software constitutes patent infringement."
+*/
+
+		"소프트웨어가 특허 침해에 해당한다고 주장하는 자에 대해 특허 소송을 제기."
 		);
 
 ?>
