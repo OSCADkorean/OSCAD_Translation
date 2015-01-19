f7c69f2af94d8b20f5c64f71841f91885e83df4a
김재경 <bonoxtwo@gmail.com>
2015-01-19 10:49:57 +0900

    김재경 snimoli/unmodified/embedded/2others/binaries/

--- backends/generic-php.be/snimoli/unmodified/embedded/2others/binaries/ms-pl-c2-inc.php
+++ backends/generic-php.be/snimoli/unmodified/embedded/2others/binaries/ms-pl-c2-inc.php
@@ -17,6 +17,22 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
+/******************************************************
+<Open Source Compliance Advisor Korean Project>
+# Contributor :
+  Junehyun Baek <100aniv62@gmail.com>
+  Dosol choi    <dosa1572@gmail.com>
+  Booyong shin  <tlsqndyd@gmail.com>
+  Hoonmoo ryu   <tnsanzz@gmail.com>
+  Minhyung Cho  <designer.mhc@gmail.com>
+  Jaekyung Kim  <bonoxtwo@gmail.com>
+  Changeun Cho  <jceun57@gmail.com>
+
+# Editor : Jaekyung Kim <bonoxtwo@gmail.com>
+# Project Period : 2015.01.12 ~ 2015.01.23
+# Edit date : 2015.01.19
+# Etc. Comment : no version changing
+*******************************************************/
 
 $gFbLicenseName="MS-PL";
 $gFbLicenseSpecification="Microsoft Public License";
@@ -32,40 +48,46 @@ $gOsLicTlExplanationChapter="6.12.9";
 $gFbLiSpUcName="MS-PL-C2";
 
 $gFbLiSpUcDesc=
-	"that you received MS-PL licensed software which you are now going to
+/*	"that you received MS-PL licensed software which you are now going to
   distribute to third parties in the form of unmodified binaries or as unmodifed
   source code files. In this case it makes no difference if you distribute a
   program, an application, a server, a snippet, a module, a library, or a plugin
-  as an independent package.";
+  as an independent package.";*/
+  "MS-PL로 라이선스 된 받은 소프트웨어의 비수정 버전을 바이너리 형태로 또는 소스코드 파일로 제3자에게 배포하고자 하는 의도를 갖고 있다. 이 경우 프로그램, 어플리케이션, 서버, 스니펫, 모듈, 라이브러리, 플러그 또는 독립패키지 등 배포유형을 식별하는 것은 아니다.";
 
 $gFbLiSpUcRequiresPrefix=
-	"the following tasks in order to fulfill the license conditions:";
+/*	"the following tasks in order to fulfill the license conditions:";*/
+"라이선스 조건을 충족하기 위한 작업은 다음과 같다.:";
 
 $gFbLiSpUcNoTasks="";
 
 $gFbLiSpUcRequiresManArr=array(
 
-  "Ensure that all licensing elements (particularly all
+/*  "Ensure that all licensing elements (particularly all
       copyright, patent, trademark, and attribution notices that are part of the
-      version you received) are completely retained in your package.",
+      version you received) are completely retained in your package.",*/
+	"라이선싱하는 요소, 특히 받은 버전의 일부인 귀속 공지, 상표, 특허, 저작권은 패키지에 전부 보유한다.",
 
-  "Incorporate a complete copy of the MS-PL license
+/*  "Incorporate a complete copy of the MS-PL license
   into your package, regardless whether you distribute a source code or a binary
-  package."
+  package."*/
+  "소스코드 또는 바이너리 패키지를 배포하는 여부와 관계없이 MS-PL 라이선스 사본을 패키지에 전부 포함한다."
 
 
 );
 
 $gFbLiSpUcRequiresVolArr=array(
-	"It's a good tradition to let the documentation of
+/*	"It's a good tradition to let the documentation of
   your distribution and/or your additional material also contain a link to the
-  original software (project) and its homepage."
+  original software (project) and its homepage."*/
+  "배포 문서 및 추가 자료에 또한 초기 소프트웨어(프로젝트)와 그 홈페이지 링크를 포함시키는 것이 바람직하다."
 );
 
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
-		"to use any contributors' name, logo, or trademarks (without an
-				additional or general legally based approval)"
+/*		"to use any contributors' name, logo, or trademarks (without an
+				additional or general legally based approval)"*/
+				"추가적인 승인 또는 법에 의한 승인 없이 기여자의 이름, 로고 또는 상표를 사용."
 		);
 
 ?>
