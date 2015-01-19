01ecc4ca76d703316cbbef604ac88f169eece140
changeun <jceun57@gmail.com>
2015-01-19 14:34:31 +0900

    김재경 snimoli/unmodified/independent/2others/sources

--- backends/generic-php.be/snimoli/unmodified/independent/2others/sources/lgpl-c2-inc.php
+++ backends/generic-php.be/snimoli/unmodified/independent/2others/sources/lgpl-c2-inc.php
@@ -16,6 +16,22 @@
 
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *****************************************************
+ *  <Open Source Compliance Advisor Korean Project>
+ *  Contributor : 
+    Junehyun Baek <100aniv62@gmail.com>
+    Dosol choi    <dosa1572@gmail.com>
+    Booyong shin  <tlsqndyd@gmail.com>
+    Hoonmoo ryu   <tnsanzz@gmail.com>
+    Minhyung Cho  <designer.mhc@gmail.com>
+    Jaekyung Kim  <bonoxtwo@gmail.com>
+    Changeun Cho  <jceun57@gmail.com>
+
+ *  Editor : Jaekyung Kim  <bonoxtwo@gmail.com>
+ *  Project Period : 2015.01.12 ~ 2015.01.23
+ *  Edit date : 2015.01.19
+ *  Etc. Comment : LGPL(KOR) -> LGPL-2.1(ENG)
+ *****************************************************
  */
 
 $gFbLicenseName="LGPLv[2.1|3.0]";
@@ -33,20 +49,26 @@ $gFbLiSpUcName="LGPL-C2";
 
 
 $gFbLiSpUcDesc=
-	"that you received LGPL-2.1 resp. LGPL-3.0 licensed software which
+	/*"that you received LGPL-2.1 resp. LGPL-3.0 licensed software which
 you are now going to distribute to third parties in the form of unmodified
 source code files or as unmodified source code package. In this case it makes no
   difference if you distribute a program, an application, a server, a snippet, a
-  module, a library, or a plugin as an independent or as an embedded unit.";
+  module, a library, or a plugin as an independent or as an embedded unit.";*/
+  
+  "LGPL-2.1 및 LGPL-3.0으로 라이선스 된 받은 소프트웨어의 비수정 버전을 소스코드 파일 형태 또는 소스코드 패키지로 제3자에게 배포하고자 하는 의도를 갖고 있다. 이 경우 독립 또는 내장 유닛으로 프로그램, 어플리케이션, 서버, 스니펫, 모듈, 라이브러리 또는 플러그인 등 배포유형을 식별하는 것은 아니다.";
+  
 
 $gFbLiSpUcRequiresPrefix=
-	"the following tasks in order to fulfill the license conditions:";
+	/*"the following tasks in order to fulfill the license conditions:";*/
+	
+	"라이선스 조건을 충족하기 위한 작업은 다음과 같다:";
+	
 
 $gFbLiSpUcNoTasks="";
 
 $gFbLiSpUcRequiresManArr=array(
 
-  "Ensure that the licensing elements - esp. all
+  /*"Ensure that the licensing elements - esp. all
   notices that refer to the LGPL-2.1 or LGPL-3.0 and to the absence of any
   warranty - are retained in your package in the form you have received them.",
 
@@ -62,22 +84,34 @@ $gFbLiSpUcRequiresManArr=array(
   "Give the recipient a copy of the LGPL-2.1 resp.
   LGPL-3.0 license. If it is not already part of the software package, add
   it."
+*/
+
+"라이선싱하는 요소, 특히 LGPL-2.1 또는 LGPL-3.0과 보증 없음을 뜻하는 공지는 받았던 형태로 패키지에 포함한다.",
+
+"배포된 소스코드 패키지에는 적절히 설계된 명확하고 쉽게 찾아볼 수 있는 저작권 공지와 보증 제한을 포함한다. 이 요소들이 누락되었다면, 각각의 라이선스 LGPL-2.1과 LGPL-3.0에 의해 문자대로 정의된 형태로 주요 저작권 공지와 보증 제한을 포함하는 파일을 새로 추가한다. (라이선스 일부가 되더라도 그리고 받는 사람에게 라이선스 자체를 양도해야 하더라도 그 권리 포기를 반복한다.)",
+
+"받는 사람에게 LGPL-2.1 및 LGPL-3.0 라이선스 사본을 각 각 제공한다. 이미 구성한 소프트웨어 패키지의 일부에서 빠져있다면 이를 추가한다."
 
 
 );
 
 $gFbLiSpUcRequiresVolArr=array(
 
-  "Let the documentation of your distribution and/or
+  /*"Let the documentation of your distribution and/or
   your additional material also reproduce the content of the existing
   copyright notices, a hint to the software name, a link to its homepage,
   the respective disclaimer of warranty, and a link to the LGPL-2.1 resp.
   LGPL-3.0.",
 
-  "Retain all existing copyright notices."
+  "Retain all existing copyright notices."*/
+  
+  "배포 문서 및 추가 자료를 통해 또한 기존의 저작권 공지 내용, 소프트웨어 명칭에 대한 힌트, 홈페이지 링크, 각 보증 제한, 각 LGPL-2.1 및 LGPL-3.0 라이선스 링크를 복제한다.",
+  
+  "기존의 모든 저작권 공지와 라이선싱하는 요소를 유지한다."
+  
 );
 
-$gFbLiSpUcForbidsPrefix="nothing explicitly with respect to this use case.";
+$gFbLiSpUcForbidsPrefix=/*"nothing explicitly with respect to this use case.";*/"이 사용 사례와 관련하여 명확한 금지사항 없음.";
 $gFbLiSpUcForbidsArr=array();
 
 ?>
--- backends/generic-php.be/snimoli/unmodified/independent/2others/sources/pgl-c2-inc.php
+++ backends/generic-php.be/snimoli/unmodified/independent/2others/sources/pgl-c2-inc.php
@@ -16,6 +16,23 @@
 
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *****************************************************
+ *  <Open Source Compliance Advisor Korean Project>
+ *  Contributor : 
+    Junehyun Baek <100aniv62@gmail.com>
+    Dosol choi    <dosa1572@gmail.com>
+    Booyong shin  <tlsqndyd@gmail.com>
+    Hoonmoo ryu   <tnsanzz@gmail.com>
+    Minhyung Cho  <designer.mhc@gmail.com>
+    Jaekyung Kim  <bonoxtwo@gmail.com>
+    Changeun Cho  <jceun57@gmail.com>
+
+ *  Editor : Jaekyung Kim  <bonoxtwo@gmail.com>
+ *  Project Period : 2015.01.12 ~ 2015.01.23
+ *  Edit date : 2015.01.19
+ *  Etc. Comment : PGL(KOR) -> PostgreSQL(ENG)
+				   Postgres(KOR) -> PostgreSQL(ENG)
+ *****************************************************
  */
 
 $gFbLicenseName="PostgreSQL";
@@ -32,32 +49,44 @@ $gOsLicTlExplanationChapter="6.13.6";
 $gFbLiSpUcName="PostgreSQL-C2";
 
 $gFbLiSpUcDesc=
-	"that you received PostgreSQL licensed software which you are now going to
+	/*"that you received PostgreSQL licensed software which you are now going to
   distribute to third parties in the form of unmodified binaries or as unmodifed
   source code files. In this case it makes no difference if you distribute a
   program, an application, a server, a snippet, a module, a library, or a plugin
-  as an independent package.";
+  as an independent package.";*/
+  
+  "PostgreSQL로 라이선스 된 받은 소프트웨어의 비수정 버전을 바이너리 형태로 또는 소스코드 파일로 제3자에게 배포하고자 하는 의도를 갖고 있다. 이 경우 프로그램, 어플리케이션, 서버, 스니펫, 모듈, 라이브러리, 플러그 또는 독립패키지 등 배포유형을 식별하는 것은 아니다.";
+  
 
 $gFbLiSpUcRequiresPrefix=
-	"the following tasks in order to fulfill the conditions of the
-	 $gFbLicenseSpecification";
+	/*"the following tasks in order to fulfill the conditions of the
+	 $gFbLicenseSpecification";*/
+	 
+	 "라이선스 조건을 충족하기 위한 작업은 다음과 같다:";
+	 
 
 $gFbLiSpUcNoTasks="";
 
 $gFbLiSpUcRequiresManArr=array(
 
-  "Ensure that the complete PostgreSQL license including
+  /*"Ensure that the complete PostgreSQL license including
   the copyright notice, the permission notices, and the PostgreSQL disclaimer
-  are retained in your package in the form you have received them."
+  are retained in your package in the form you have received them."*/
+  
+  "저작권 공지, 허가 공지 및 PostgreSQL 권리포기사항을 포함하여 PostgreSQL 라이선스를 받았던 형태로 패키지에 전부 보유한다."
+  
 );
 
 $gFbLiSpUcRequiresVolArr=array(
-  "It's a good tradition to let the documentation of
+  /*"It's a good tradition to let the documentation of
   your distribution and/or your additional material also contain a link to the
-  original software (project) and its homepage."
+  original software (project) and its homepage."*/
+  
+  "배포 문서 및 추가 자료에 또한 초기 소프트웨어(프로젝트)와 그 홈페이지 링크를 포함시키는 것이 바람직하다."
+  
 );
 
-$gFbLiSpUcForbidsPrefix="nothing explicitly.";
+$gFbLiSpUcForbidsPrefix=/*"nothing explicitly.";*/"명시된 사항 없음.";
 $gFbLiSpUcForbidsArr=array();
 
 ?>
