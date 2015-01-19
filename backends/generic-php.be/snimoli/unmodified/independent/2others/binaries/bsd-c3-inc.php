049480f0ed4c0db5c3cdd03221176a12114b7891
김재경 <bonoxtwo@gmail.com>
2015-01-19 17:28:51 +0900

    김재경 snimoli/unmodified/independent/2others/binaries/

--- backends/generic-php.be/snimoli/unmodified/independent/2others/binaries/bsd-c3-inc.php
+++ backends/generic-php.be/snimoli/unmodified/independent/2others/binaries/bsd-c3-inc.php
@@ -16,6 +16,22 @@
 
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *****************************************************
+ *  <Open Source Compliance Advisor Korean Project>
+ *  Contributor : 
+    Junehyun Baek <100aniv62@gmail.com>
+    Dosol choi    <dosa1572@gmail.com>
+    Booyong shin  <tlsqndyd@gmail.com>
+    Hoonmoo ryu   <tnsanzz@gmail.com>
+    Minhyung Cho  <designer.mhc@gmail.com>
+    Jaekyung Kim  <bonoxtwo@gmail.com>
+    Changeun Cho  <jceun57@gmail.com>
+
+ *  Editor : Jaekyung Kim  <bonoxtwo@gmail.com>
+ *  Project Period : 2015.01.12 ~ 2015.01.23
+ *  Edit date : 2015.01.19
+ *  Etc. Comment : Apache(KOR) -> Apache-2.0(ENG)
+ *****************************************************
  */
 
 $gFbLicenseName="BSDv[2|3]CL";
@@ -32,30 +48,34 @@ $gOsLicTlExplanationChapter="6.4.10";
 $gFbLiSpUcName="BSD-C3";
 
 $gFbLiSpUcDesc=
-"that you received BSD licensed software which you are now
+/*"that you received BSD licensed software which you are now
   going to distribute to third parties in the form of unmodified binary files or
   as unmodified binary package. In this case it does not matter if you distribute
   a program, an application, a server, a snippet, a module, a library, or a
-  plugin as an independent or an embedded unit.";
+  plugin as an independent or an embedded unit.";*/
+  "BSD로 라이선스 된 받은 소프트웨어의 비수정 버전을 바이너리 파일 형태 또는 바이너리 패키지로 제3자에게 배포하려는 의도를 갖고 있다. 이 경우 독립 또는 내장 유닛으로 프로그램, 어플리케이션, 서버, 스니펫, 모듈, 라이브러리 또는 플러그인 등 배포유형을 식별하는 것은 아니다.";
 
 $gFbLiSpUcRequiresPrefix=
-	"the following tasks in order to fulfill the license conditions:";
+/*	"the following tasks in order to fulfill the license conditions:";*/
+"라이선스 조건을 충족하기 위한 작업은 다음과 같다.:";
 
 $gFbLiSpUcNoTasks="";
 
 $gFbLiSpUcRequiresManArr=array(
 
-		"Ensure that your distribution contains the original copyright notice,
+/*		"Ensure that your distribution contains the original copyright notice,
 		the BSD license, and the BSD disclaimer in the form  you have received them.
 		If you compile the binary file on the base of the source code package and
 		if this compilation does not also generate and integrate the licensing
 		files then create the copyright notice, the BSD conditions, and the BSD
 		disclaimer according to the form of the source code package and insert
-		these files into your distribution manually.",
+		these files into your distribution manually.",*/
+		"배포물에 받았던 형태로 최초 저작권 공지, BSD 라이선스, BSD 권리포기사항을 포함한다. 소스코드 패키지에 기반하여 바이너리 파일을 컴파일하고 이 컴파일로 라이선싱 파일이 생성되거나 포함되지 않는다면, 소스코드 패키지 형태에 따라 저작권 공지, BSD 조건, BSD 권리포기사항을 작성한 다ㅡㅁ에 이 파일들을 배포물에 수동으로 삽입한다.",
 
-		"Ensure that the documentation of your distribution and/or your additional
+/*		"Ensure that the documentation of your distribution and/or your additional
 		material also contain the author specific copyright notice, the BSD
-		conditions, and the BSD disclaimer."
+		conditions, and the BSD disclaimer."*/
+		"배포 문서 및 추가 자료는 최초 저작권 공지, BSD 조건, BSD 권리포기사항을 포함한다."
 );
 
 $gFbLiSpUcRequiresVolArr=array(
@@ -63,10 +83,11 @@ $gFbLiSpUcRequiresVolArr=array(
 
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
-		"nothing explicitly if you are using the BSD 2 Clause License. But the
+/*		"nothing explicitly if you are using the BSD 2 Clause License. But the
 		BSD 3 Clause License explicitly prohibits to use the name of the
 		licensing organization or the names of the licensing contributors to
-		promote your own work."
+		promote your own work."*/
+		"BSD 2 Clause License를 사용하고 있다면 금지사항은 명확히 존재하지 않는다. 그러나 BSD 3 Clause License에 의해 자신의 저작물을 홍보하기 위해 라이선싱 기관명 또는 라이선싱 기여자명 사용이 명백히 금지된다."
 		);
 
 ?>
--- backends/generic-php.be/snimoli/unmodified/independent/2others/binaries/osuc-05b-inc.php
+++ backends/generic-php.be/snimoli/unmodified/independent/2others/binaries/osuc-05b-inc.php
@@ -16,6 +16,22 @@
 
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *****************************************************
+ *  <Open Source Compliance Advisor Korean Project>
+ *  Contributor : 
+    Junehyun Baek <100aniv62@gmail.com>
+    Dosol choi    <dosa1572@gmail.com>
+    Booyong shin  <tlsqndyd@gmail.com>
+    Hoonmoo ryu   <tnsanzz@gmail.com>
+    Minhyung Cho  <designer.mhc@gmail.com>
+    Jaekyung Kim  <bonoxtwo@gmail.com>
+    Changeun Cho  <jceun57@gmail.com>
+
+ *  Editor : Jaekyung Kim  <bonoxtwo@gmail.com>
+ *  Project Period : 2015.01.12 ~ 2015.01.23
+ *  Edit date : 2015.01.19
+ *  Etc. Comment : Apache(KOR) -> Apache-2.0(ENG)
+ *****************************************************
  */
 
 $gFbOsucNumber="05B";
@@ -23,10 +39,11 @@ $gFbOsucName="OSUC-".$gFbOsucNumber;
 
 
 $gFbOsucDesc=
-"Just as you received it, you are going to distribute an unmodified open source
+/*"Just as you received it, you are going to distribute an unmodified open source
 library, code snippet, module, or plugin to 3rd parties - in the form of
 binaries. In this act of distribution, you do not combine this library, code
 snippet, module, or plugin with other software components in the sense of
-software development";
+software development";*/
+"받은 그대로 바이너리 형태로 제3자에게 수정되지 않은 오픈소스 라이브러리, 스니펫 코드, 모듈 또는 플러그인을 배포할 의도를 갖고 있다. 이와 같은 배포 행위에서는 소프트웨어 개발이라는 취지로 이 스니펫 코드, 모듈 또는 플러그인을 다른 소프트웨어 컴포넌트와 결합하지 않는다. 구체적으로 라이선스를 충족시키는 과업 목록을 살펴보려면 아래의 안내하는 쪽으로 이동한다.";
 
 ?>
