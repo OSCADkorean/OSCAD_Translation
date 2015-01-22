52f5d36726b4a91ca2fad47bd0d50301b1aece96
류훈무 <tnsanzz@gmail.com>
2015-01-22 10:10:50 +0900

    류훈무 \master-files\lsuc-master-files\php

--- backends/generic-php.be/master-files/lsuc-master-files/php/php-c2-inc-master.php
+++ backends/generic-php.be/master-files/lsuc-master-files/php/php-c2-inc-master.php
@@ -17,7 +17,22 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
+ /******************************************************
+<Open Source Compliance Advisor Korean Project>
+# Contributor :
+  Junehyun Baek <100aniv62@gmail.com>
+  Dosol choi    <dosa1572@gmail.com>
+  Booyong shin  <tlsqndyd@gmail.com>
+  Hoonmoo ryu   <tnsanzz@gmail.com>
+  Minhyung Cho  <designer.mhc@gmail.com>
+  Jaekyung Kim  <bonoxtwo@gmail.com>
+  Changeun Cho  <jceun57@gmail.com>
 
+# Editor : ryu hoon moo <tnsanzz@gmail.com>
+# Project Period : 2015.01.12 ~ 2015.01.23
+# Edit date : 2015.01.22
+# Etc. Comment : 
+*******************************************************/ 
 $gFbLicenseName="PHP-3.0";
 $gFbLicenseSpecification="PHP-3.0 License";
 $gFbLicenseAbbr="PHP";
@@ -32,20 +47,29 @@ $gOsLicTlExplanationChapter="6.14.10";
 $gFbLiSpUcName="PHP-3.0-C2";
 
 $gFbLiSpUcDesc=
-	"that you received PHP-3.0 licensed software which you are now
+/*	"that you received PHP-3.0 licensed software which you are now
   going to distribute to third parties in the form of unmodified source code
   files or as unmodified source code package. In this case it makes no
   difference if you distribute a program, an application, a server, a snippet, a
   module, a library, or a plugin as an independent or as an embedded unit.";
+*/
+	
+" PHP-3.0으로 라이선스 된 받은 소프트웨어의 비수정 버전을 소스코드 파일 형태 또는 소스코드 패키지로 제3자에게 배포하고자 하는 의도를 갖고 있다. 이 경우 독립 또는 내장 유닛으로 프로그램, 어플리케이션, 서버, 스니펫, 모듈, 라이브러리 또는 플러그인 등 배포유형을 식별하는 것은 아니다. ";
+
 
 $gFbLiSpUcRequiresPrefix=
-	"the following tasks in order to fulfill the license conditions:";
+/*	"the following tasks in order to fulfill the license conditions:";
+*/
+	
+" 라이선스 조건을 충족하기 위한 작업은 다음과 같다.
+";
+
 
 $gFbLiSpUcNoTasks="";
 
 $gFbLiSpUcRequiresManArr=array(
 
-	"Ensure that the complete PHP-3.0 license - esp. the copyright notice,
+/*	"Ensure that the complete PHP-3.0 license - esp. the copyright notice,
 	the PHP-3.0 conditions, and the PHP-3.0 disclaimer - are retained in your
 		package in the form you have received them.",
 
@@ -54,18 +78,32 @@ $gFbLiSpUcRequiresManArr=array(
 	includes PHP, freely available from http://www.php.net/'."
 
 );
+*/
+	
+"전체 PHP-3.0 라이선스(특히, 저작권 공지, PHP-3.0 조건 및 PHP-3.0 권리포기)는 받았던 형태로 패키지에 포함한다.",	
+	
+" 배포 문서 및 추가 자료에 “이 제품은 다음의 주소(htp:/www.php.net/)에서 자유로이 이용할 수 있는 PHP를 포함한다”라는 형태로 승인 문구를 포함한다.");
+
 
 $gFbLiSpUcRequiresVolArr=array(
-	"Let the documentation of your distribution and/or
+/*	"Let the documentation of your distribution and/or
   your additional material also contain the original copyright notice, the PHP-3.0
   conditions, and the PHP-3.0 disclaimer."
 );
+*/
+	
+" 배포 문서 및 추가 자료에 또한 초기 저작권 공지, PHP-3.0 조건 및 PHP-3.0 권리포기사항을 포함한다. ");
+
 
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
-		"to endorse or promote your product by mentioning PHP, esp. not
+/*		"to endorse or promote your product by mentioning PHP, esp. not
 by making the string 'PHP' part of its name."
 		);
+
+*/
+	
+" ‘PHP’라는 명칭으로 단독으로 사용된 소프트웨어에 근거하여 어떠한 서비스를 보증하거나 홍보하는 행위");
 
 
 ?>
