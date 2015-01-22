9004368466d7d0bea5ea2c8cf8828df30e9d4eff
junehyunBaek <100aniv62@gamil.com>
2015-01-22 16:38:48 +0900

    php test

--- backends/generic-php.be/master-files/lsuc-master-files/php/php-c8-inc-master.php
+++ backends/generic-php.be/master-files/lsuc-master-files/php/php-c8-inc-master.php
@@ -17,22 +17,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
- /******************************************************
-<Open Source Compliance Advisor Korean Project>
-# Contributor :
-  Junehyun Baek <100aniv62@gmail.com>
-  Dosol choi    <dosa1572@gmail.com>
-  Booyong shin  <tlsqndyd@gmail.com>
-  Hoonmoo ryu   <tnsanzz@gmail.com>
-  Minhyung Cho  <designer.mhc@gmail.com>
-  Jaekyung Kim  <bonoxtwo@gmail.com>
-  Changeun Cho  <jceun57@gmail.com>
 
-# Editor : ryu hoon moo <tnsanzz@gmail.com>
-# Project Period : 2015.01.12 ~ 2015.01.23
-# Edit date : 2015.01.22
-# Etc. Comment : 
-*******************************************************/ 
 $gFbLicenseName="PHP-3.0";
 $gFbLicenseSpecification="PHP-3.0 License";
 $gFbLicenseAbbr="PHP";
@@ -47,29 +32,20 @@ $gOsLicTlExplanationChapter="6.14.10";
 $gFbLiSpUcName="PHP-3.0-C8";
 
 $gFbLiSpUcDesc=
-/*	"that you received a PHP-3.0 licensed code snippet, module, library,
+	"that you received a PHP-3.0 licensed code snippet, module, library,
   or plugin (snimoli), that you modified it, and that you are now going to
   distribute this modified version to third parties in the form of source code
   files or as a source code package together with another larger software unit
   which contains this code snippet, module, library, or plugin as an embedded
   component.";
-*/
-	
-" PHP-3.0로 라이선스 된 받은 스니펫, 모듈, 라이브러리 또는 플러그인 등 스니몰리의 수정 버전을 소스코드 파일 형태 또는 소스코드 패키지로 제3자에게 내장 컴포넌트로서 스니펫, 모듈, 라이브러리 또는 플러그인을 포함하는 다른 더 큰 소프트웨어 유닛과 함께 배포하려는 의도를 갖고 있다.";
-
 
 $gFbLiSpUcRequiresPrefix=
-/*	"the following tasks in order to fulfill the license conditions:";
-*/
-	
-" 라이선스 조건을 충족하기 위한 작업은 다음과 같다.
-";
-
+	"the following tasks in order to fulfill the license conditions:";
 
 $gFbLiSpUcNoTasks="";
 
 $gFbLiSpUcRequiresManArr=array(
-/*
+
   "Ensure that the complete PHP-3.0 license - esp.
   the copyright notice, the PHP-3.0 conditions, and the PHP-3.0 disclaimer - are
   retained in your package in the form you have received them.",
@@ -82,21 +58,9 @@ $gFbLiSpUcRequiresManArr=array(
   your additional material also contain the original copyright notice, the PHP-3.0
   conditions, and the PHP-3.0 disclaimer."
 		);
-*/
-	
-"전체 PHP-3.0 라이선스(특히, 저작권 공지, PHP-3.0 조건 및 PHP-3.0 권리포기)는 받았던 형태로 패키지에 포함한다.",
-
-
-	
-"배포 문서 및 추가 자료에 “이 제품은 다음의 주소(htp:/www.php.net/)에서 자유로이 이용할 수 있는 PHP를 포함한다”라는 형태로 승인 문구를 포함한다.",
-
-
-	
-"배포 문서 및 추가 자료에 또한 초기 저작권 공지, PHP-3.0 조건 및 PHP-3.0 권리포기사항을 포함한다.");
-
 
 $gFbLiSpUcRequiresVolArr=array(
-/*
+
   "It is a good practice of the open source
   community, to let the copyright notice which is shown by the running program
   also state that the program uses a copmponent licensed under the PHP-3.0 license.
@@ -115,32 +79,12 @@ $gFbLiSpUcRequiresVolArr=array(
   libraries, modules, snippets, or plugins in specific directory which contains
   also all additional licensing elements."
 );
-*/
-	
-
-	
-"실행 프로그램에 의해 표시되는 저작권 공지에서는 PHP-3.0 라이선스에 기반하여 사용 허가된 컴포넌트를 프로그램이 사용한다는 설명을 밝히는 것이 오픈소스 커뮤니티에서는 바람직한 행위이다. 지배적인 프로그램의 저작권 화면을 통해 저작권 공지, PHP-3.0 조건 및 PHP-3.0 권리포기사항을 포함하여 전체 PHP-3.0 라이선스를 복제한다(바이너리 배포 시 요구됨)",
-
-
-
-
-	
-"소스코드 안에 수정사항을 표시한다.",
-
-
-	
-" 라이선싱하는 요소(특히, PHP-3.0 라이선스 텍스트, 원저작자 특정 저작권 공지 및 PHP-3.0 권리포기사항)가 내장 라이브러리만을 명확히 가리키고 자신의 지배적인 저작물의 라이선싱을 침해하지 않도록 소스코드 배포물을 구성한다. 모든 라이선싱하는 요소가 들어간 특정 디렉토리에 라이브러리, 모듈, 스니펫 또는 플러그인과 같은 내장 컴포넌트를 두는 것이 바람직하다.");
-
 
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
-/*		"to endorse or promote your product by mentioning PHP, esp. not
+		"to endorse or promote your product by mentioning PHP, esp. not
 by making the string 'PHP' part of its name."
 		);
-
-*/
-	
-" ‘PHP’라는 명칭으로 단독으로 사용된 소프트웨어에 근거하여 어떠한 서비스를 보증하거나 홍보하는 행위");
 
 
 ?>
