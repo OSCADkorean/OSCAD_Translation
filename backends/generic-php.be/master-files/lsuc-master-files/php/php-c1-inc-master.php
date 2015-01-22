9b08b68d4b189ba1dd44830ff7f659ba06e3c99a
junehyunBaek <100aniv62@gamil.com>
2015-01-22 16:28:59 +0900

    php 수정본

--- backends/generic-php.be/master-files/lsuc-master-files/php/php-c1-inc-master.php
+++ backends/generic-php.be/master-files/lsuc-master-files/php/php-c1-inc-master.php
@@ -1,5 +1,4 @@
-<?PHP-3.0
-
+<?php
 /*  This file is part of OSCAd, the Open Source Compliance Advisor
  *
  *  Copyright (C) 2013 Karsten Reincke, Deutsche Telekom AG
@@ -8,16 +7,14 @@
  *  it under the terms of the GNU Affero General Public License as
  *  published by the Free Software Foundation, either version 3 of the
  *  License, or (at your option) any later version.
-
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Affero General Public License for more details.
-
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
- /******************************************************
+/******************************************************
 <Open Source Compliance Advisor Korean Project>
 # Contributor :
   Junehyun Baek <100aniv62@gmail.com>
@@ -27,69 +24,44 @@
   Minhyung Cho  <designer.mhc@gmail.com>
   Jaekyung Kim  <bonoxtwo@gmail.com>
   Changeun Cho  <jceun57@gmail.com>
-
 # Editor : ryu hoon moo <tnsanzz@gmail.com>
 # Project Period : 2015.01.12 ~ 2015.01.23
 # Edit date : 2015.01.22
-# Etc. Comment : 
-*******************************************************/ 
-
+*******************************************************/  
 $gFbLicenseName="PHP-3.0";
-<<<<<<< HEAD
 $gFbLicenseSpecification="PHP-3.0 License";
 $gFbLicenseAbbr="PHP";
-=======
-$gFbLicenseSpecification="PHP-3.0-3.0 License";
-$gFbLicenseAbbr="PHP-3.0";
->>>>>>> branch 'master' of http://jceun57@intrepo.cafe24.com:9000/Koscom/Koscom
 $gFbLicenseRelease="3.0";
-
 $gOsLicProtectionChapter="2.13";
 $gOsLicPatentChapter="";
 $gOsLicTodoListChapter="6.14";
 $gOsLicLsucChapter="6.14.1";
 $gOsLicTlExplanationChapter="6.14.10";
-
 $gFbLiSpUcName="PHP-3.0-C1";
-
 $gFbLiSpUcDesc=
 /*	"that you received PHP-3.0 licensed software, that you will use it
   only for yourself, and that you do not hand it over to any 3rd party in any
   sense.";
 */
-
-" PHP-3.0으로 라이선스 된 받은 소프트웨어를 단독으로 사용하고 제3자에게 양도하지 않는 의도를 갖고 있다. ";
-
+" PHP로 라이선스 된 받은 소프트웨어를 단독으로 사용하고 제3자에게 양도하지 않는 의도를 갖고 있다.";
 $gFbLiSpUcRequiresPrefix=
-/*		"no tasks in order to fulfill the conditions of the PHP-3.0 license
+/*	"no tasks in order to fulfill the conditions of the PHP-3.0 license
 	with respect to this use case:";
 */
-
-	
-" 다음 사용 사례에 관련하여 PHP-3.0 라이선스 조건을 충족하기 위한 작업은 없다";
-
-
+" 다음 사용 사례에 관련하여 PHP 라이선스 조건을 충족하기 위한 작업은 없다.";
 $gFbLiSpUcNoTasks=
-/*		"You are allowed to use any kind of PHP-3.0 software in any sense and in
+/*	"You are allowed to use any kind of PHP-3.0 software in any sense and in
     any context without any obligations as long as you do not give the software
     to 3rd parties.";
-*/	
-	
-" 제3자에게 소프트웨어를 제공하지 않는 한 어떠한 의무가 부과되지 않는 정황에서 어떠한 유형의 PHP-3.0 소프트웨어를 사용할 수 있다. ";
-
-
+*/
+"제3자에게 소프트웨어를 제공하지 않는 한 어떠한 의무가 부과되지 않는 정황에서 어떠한 유형의 PHP 소프트웨어를 사용할 수 있다. ";
 $gFbLiSpUcRequiresManArr=array();
-
 $gFbLiSpUcRequiresVolArr=array();
-
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
-/*			"to endorse or promote any service you establish on the base of
+/*		"to endorse or promote any service you establish on the base of
 		this privately used software by the name 'PHP'."
 		);
-*/	
-	
+*/
 " ‘PHP’라는 명칭으로 단독으로 사용된 소프트웨어에 근거하여 어떠한 서비스를 보증하거나 홍보하는 행위");
-
-
 ?>
