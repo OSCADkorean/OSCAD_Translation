4c5beaeac75e887a7929424ae7ce597fcd63f6f5
junehyunBaek <100aniv62@gamil.com>
2015-01-23 09:36:18 +0900

    php 수정

--- backends/generic-php.be/master-files/lsuc-master-files/php/php-c5-inc-master.php
+++ backends/generic-php.be/master-files/lsuc-master-files/php/php-c5-inc-master.php
@@ -17,7 +17,21 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
+/******************************************************
+<Open Source Compliance Advisor Korean Project>
+# Contributor :
+  Junehyun Baek <100aniv62@gmail.com>
+  Dosol choi    <dosa1572@gmail.com>
+  Booyong shin  <tlsqndyd@gmail.com>
+  Hoonmoo ryu   <tnsanzz@gmail.com>
+  Minhyung Cho  <designer.mhc@gmail.com>
+  Jaekyung Kim  <bonoxtwo@gmail.com>
+  Changeun Cho  <jceun57@gmail.com>
 
+# Editor : Buyong Shin <tlsqndyd@gmail.com>
+# Project Period : 2015.01.12 ~ 2015.01.23
+# Edit date : 2015.01.23
+*******************************************************/  
 $gFbLicenseName="PHP-3.0";
 $gFbLicenseSpecification="PHP-3.0 License";
 $gFbLicenseAbbr="PHP";
@@ -32,18 +46,21 @@ $gOsLicTlExplanationChapter="6.14.10";
 $gFbLiSpUcName="PHP-3.0-C5";
 
 $gFbLiSpUcDesc=
+/*
 	"that you received a PHP-3.0 licensed program, application, or
   server (proapse), that you modified it, and that you are now going to
   distribute this modified version to third parties in the form of binary files or as a
   binary package.";
-
+*/
+"PHP로 라이선스 된 받은 프로그램, 어플리케이션 또는 서버 등 프로앱스의 수정 버전을 바이너리파일 형태 또는 바이너리 패키지로 제3자에게 배포하려는 의도를 갖고 있다.";
 $gFbLiSpUcRequiresPrefix=
-	"the following tasks in order to fulfill the license conditions:";
-
+	/*"the following tasks in order to fulfill the license conditions:";
+*/
+"라이선스 조건을 충족하기 위한 작업은 다음과 같다.:";
 $gFbLiSpUcNoTasks="";
 
 $gFbLiSpUcRequiresManArr=array(
-
+/*
 		"Let the documentation of your distribution and/or your additional
 		material also contain a line of acknowledgment in the form 'This product
 		includes PHP, freely available from http://www.php.net/'.",
@@ -53,9 +70,14 @@ $gFbLiSpUcRequiresManArr=array(
 		conditions, and the PHP-3.0 disclaimer.",
 
 );
+*/
+	"배포 문서 및 추가 자료에 “이 제품은 다음의 주소(htp:/www.php.net/)에서 자유로이이용할 수 있는 PHP를 포함한다”라는 형태로 승인 문구를 포함한다",
 
+		"배포 문서 및 추가 자료에 또한 초기 저작권 공지, PHP 조건 및 PHP 권리포기사항을 포함한다..",
+
+);
 $gFbLiSpUcRequiresVolArr=array(
-
+/*
   "Ensure that the complete PHP-3.0 license - esp. the
   copyright notice, the PHP-3.0 conditions, and the PHP-3.0 disclaimer - are
   reproduced by your package. If such a notice is missed in the
@@ -67,12 +89,20 @@ $gFbLiSpUcRequiresVolArr=array(
 		even if you do not want to distribute the code."
 
 );
+*/
+ "전체 PHP 라이선스(특히, 저작권 공지, PHP 조건 및 PHP 권리포기)는 패키지를 통해 복제한다. 그런 공지가 저작권 화면에 생략되었다면 저작권 공지, PHP 조건 및 PHP 권리포기사항을 포함하여 전체 PHP 라이선스가 복제되도록 화면을 수정한다.",
 
+		"코드를 배포하지 않으려 해도 소스코드 안에 수정사항을 표시한다."
+
+);
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
-		"to endorse or promote your product by mentioning PHP, esp. not
+/*	
+	"to endorse or promote your product by mentioning PHP, esp. not
 by making the string 'PHP' part of its name."
 		);
-
+*/
+"‘PHP’라는 명칭으로 단독으로 사용된 소프트웨어에 근거하여 어떠한 서비스를 보증하거나 홍보하는 행위"
+		);
 
 ?>
--- backends/generic-php.be/master-files/lsuc-master-files/php/php-c7-inc-master.php
+++ backends/generic-php.be/master-files/lsuc-master-files/php/php-c7-inc-master.php
@@ -17,7 +17,21 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
+/******************************************************
+<Open Source Compliance Advisor Korean Project>
+# Contributor :
+  Junehyun Baek <100aniv62@gmail.com>
+  Dosol choi    <dosa1572@gmail.com>
+  Booyong shin  <tlsqndyd@gmail.com>
+  Hoonmoo ryu   <tnsanzz@gmail.com>
+  Minhyung Cho  <designer.mhc@gmail.com>
+  Jaekyung Kim  <bonoxtwo@gmail.com>
+  Changeun Cho  <jceun57@gmail.com>
 
+# Editor : Buyong Shin <tlsqndyd@gmail.com>
+# Project Period : 2015.01.12 ~ 2015.01.23
+# Edit date : 2015.01.23
+*******************************************************/  
 $gFbLicenseName="PHP-3.0";
 $gFbLicenseSpecification="PHP-3.0 License";
 $gFbLicenseAbbr="PHP";
@@ -31,20 +45,22 @@ $gOsLicTlExplanationChapter="6.14.10";
 
 $gFbLiSpUcName="PHP-3.0-C7";
 
-$gFbLiSpUcDesc=
+$gFbLiSpUcDesc=/*
 	"that you received a PHP-3.0 licensed code snippet, module, library,
   or plugin (snimoli), that you modified it, and that you are now going to
   distribute this modified version to third parties in the form of binary files
   or as a binary package but without embedding it into another larger software
-  unit.";
+  unit.";*/
+  "PHP로 라이선스 된 받은 스니펫, 모듈, 라이브러리 또는 플러그인 등 스니몰리의 수정 버전을 바이너리 파일 형태 또는 바이너리 패키지로 제3자에게 배포하려는 의도를 갖고 있다. 단, 이를 다른 더 큰 소프트웨어 유닛에 삽입하지는 않는다.";
 
 $gFbLiSpUcRequiresPrefix=
-	"the following tasks in order to fulfill the license conditions:";
+	/*"the following tasks in order to fulfill the license conditions:";*/
+	"라이선스 조건을 충족하기 위한 작업은 다음과 같다.:";
 
 $gFbLiSpUcNoTasks="";
 
 $gFbLiSpUcRequiresManArr=array(
-
+/*
   "Let the documentation of your distribution and/or your additional
 	material also contain a line of acknowledgment in the form 'This product
 	includes PHP, freely available from http://www.php.net/'.",
@@ -54,9 +70,14 @@ $gFbLiSpUcRequiresManArr=array(
   conditions, and the PHP-3.0 disclaimer."
 
 );
+*/
+"배포 문서 및 추가 자료에 “이 제품은 다음의 주소(htp:/www.php.net/)에서 자유로이이용할 수 있는 PHP를 포함한다”라는 형태로 승인 문구를 포함한다.",
 
+	"배포 문서 및 추가 자료에 또한 초기 저작권 공지, PHP 조건 및 PHP 권리포기사항을 포함한다"
+
+);
 $gFbLiSpUcRequiresVolArr=array(
-
+/*
 	"Ensure that the complete PHP-3.0 license - esp. the
 	copyright notice, the PHP-3.0 conditions, and the PHP-3.0 disclaimer - are
 	reproduced by your package - as far as this can be done by the
@@ -66,12 +87,20 @@ $gFbLiSpUcRequiresVolArr=array(
 		even if you do want to distribute the code."
 
 );
+*/
+"전체 PHP 라이선스(특히, 저작권 공지, PHP 조건 및 PHP 권리포기)는 패키지를 통해 복제한다(바이너리로 이 작업이 가능한 한)",
 
+	"코드를 배포하지 않으려 해도 소스코드 안에 수정사항을 표시한다."
+
+);
 $gFbLiSpUcForbidsPrefix="";
 $gFbLiSpUcForbidsArr=array(
+/*
 		"to endorse or promote your product by mentioning PHP, esp. not
 by making the string 'PHP' part of its name."
 		);
-
+*/
+"‘PHP’라는 명칭으로 단독으로 사용된 소프트웨어에 근거하여 어떠한 서비스를 보증하거나 홍보하는 행위"
+		);
 
 ?>
